09ab92986b7997b79df6a117fb8427e946b763ec
erickGzz <eagm080897@gmail.com>
2023-02-14 17:04:02 -0600

    fix window size function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from 'react'
-
+import { isMobile } from 'react-device-detect'
 import NavBarDesktop from './NavBarDesktop'
 import { NavBarMobile } from './NavBarMobile'
 
 const Header = () => {
-  const [isMobile, setIsMobile] = useState<boolean>(false)
-
-  const handleResize = (): void => {
-    if (window.innerWidth < 768) {
-      setIsMobile(true)
-    } else {
-      setIsMobile(false)
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
-  }, [handleResize])
-
   return <header>{!isMobile ? <NavBarDesktop /> : <NavBarMobile />}</header>
 }
 
